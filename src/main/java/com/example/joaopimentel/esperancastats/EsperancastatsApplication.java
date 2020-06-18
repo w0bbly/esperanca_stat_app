7d16e87098b7a749f6af42df68dadce9f6f966f7
w0bbly <joaopim16@gmail.com>
2020-06-18 21:16:26 +0100

    Esperanca Initial Commit

---
+++ src/main/java/com/example/joaopimentel/esperancastats/EsperancastatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.joaopimentel.esperancastats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsperancastatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsperancastatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/joaopimentel/esperancastats/EsperancastatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joaopimentel.esperancastats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsperancastatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
